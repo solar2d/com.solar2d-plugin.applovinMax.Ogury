9137d6eb6ff8a9822aa90facb78edb0e49f1b026
Scott Harrison <scottrules44@gmail.com>
2022-09-10 21:34:11 -0500

    Update to adapter for Max 11.4

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = {  },
-		frameworks = { "AppLovinMediationOguryPresageAdapter", "OguryAds", "OMSDK_Oguryco", "OguryChoiceManager", "OgurySdk" },
+		staticLibs = { "OguryPresageAdapter" },
+		frameworks = { "OguryAds", "OMSDK_Oguryco" },
 		frameworksOptional = {},
 	},
 }
